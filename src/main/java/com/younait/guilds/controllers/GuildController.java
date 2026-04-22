44130eb73eafa244a96c100aad8895d363202f71
Younes <younait.devs@gmail.com>
2026-04-22 19:49:56 +0100

    feat: add REST controllers for Guild and Warrior resources

---
+++ src/main/java/com/younait/guilds/controllers/GuildController.java
@@ -0,0 +1,69 @@
+package com.younait.guilds.controllers;
+
+import com.younait.guilds.domain.DTO.GuildDto;
+import com.younait.guilds.domain.Entities.GuildEntity;
+import com.younait.guilds.mappers.impl.GuildMapperImpl;
+import com.younait.guilds.services.GuildService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+public class GuildController {
+    private final GuildService guildService;
+    private final GuildMapperImpl guildMapper;
+
+    public GuildController(GuildService guildService, GuildMapperImpl guildMapper) {
+
+        this.guildService = guildService;
+        this.guildMapper = guildMapper;
+    }
+
+
+    @PostMapping(path = "/guilds")
+    public ResponseEntity<GuildDto> createGuild(@RequestBody GuildDto guilddto) {
+        GuildEntity guildEntity = guildMapper.mapFrom(guilddto);
+        GuildEntity savedGuild = guildService.createGuild(guildEntity);
+        return new ResponseEntity<>(guildMapper.mapTo(savedGuild), HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/guilds")
+    public List<GuildDto> ListGuilds(){
+        List<GuildEntity> guilds=guildService.findAll();
+     return guilds.stream()
+             .map(guildMapper::mapTo)
+             .collect(Collectors.toList());
+    }
+
+    @GetMapping("/guilds/{id}")
+    public ResponseEntity<GuildDto> GetGuild(@PathVariable("id") int id){
+        Optional<GuildEntity> foundGuild=guildService.findone(id);
+       return foundGuild.map(guildEntity -> {
+           GuildDto guildDto= guildMapper.mapTo(guildEntity);
+           return new ResponseEntity<>(guildDto,HttpStatus.OK);
+        }) .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+    @PutMapping("/guilds/{id}")
+    public ResponseEntity<GuildDto> fullUpdateGuild(
+            @PathVariable("id") int id,
+            @RequestBody GuildDto guildDto) {
+    boolean existingGuild=guildService.isExist(id);
+    if(!existingGuild){
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+        GuildEntity guildEntity = guildMapper.mapFrom(guildDto);
+        guildEntity.setId(id);
+        GuildEntity savedGuild = guildService.createGuild(guildEntity);
+        return new ResponseEntity<>(guildMapper.mapTo(savedGuild), HttpStatus.OK);
+
+
+    }
+
+}
+
+
---
+++ src/main/java/com/younait/guilds/controllers/WarriorController.java
@@ -0,0 +1,65 @@
+package com.younait.guilds.controllers;
+
+import com.younait.guilds.domain.DTO.WarriorDto;
+import com.younait.guilds.domain.Entities.WarriorEntity;
+import com.younait.guilds.mappers.impl.WarriorMapperImpl;
+import com.younait.guilds.services.WarriorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+public class WarriorController {
+    WarriorMapperImpl warriorMapper;
+    WarriorService warriorService;
+    public WarriorController(WarriorMapperImpl warriorMapper, WarriorService warriorService){
+        this.warriorMapper=warriorMapper;
+        this.warriorService=warriorService;
+
+    }
+
+   @PostMapping ("/warriors")
+    public ResponseEntity<WarriorDto> createbook(@RequestBody WarriorDto warriorDto){
+        WarriorEntity warriorEntity=warriorMapper.mapFrom(warriorDto);
+        WarriorEntity savedWarrior=warriorService.CreateWarrior(warriorEntity);
+        return new ResponseEntity<WarriorDto>(warriorMapper.mapTo(savedWarrior), HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("/warriors")
+    public List<WarriorDto> ListWarriors(){
+        List<WarriorEntity> lw=warriorService.ListWarriors();
+        return lw.stream()
+                .map(warriorMapper::mapTo)
+                .collect(Collectors.toList());
+
+    }
+
+    @GetMapping("/warriors/{id}")
+    public ResponseEntity<WarriorDto> getWarrior(@PathVariable("id") int id){
+        Optional<WarriorEntity> FoundWarrior=warriorService.findonewarrior(id);
+            return FoundWarrior.map(warrior -> {
+            WarriorDto warriorDto= warriorMapper.mapTo(warrior);
+            return ResponseEntity.ok(warriorDto);
+
+        }).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+@PutMapping("warriors/{id}")
+    public ResponseEntity<WarriorDto> fullUpdateGuild(@PathVariable
+        ("id") int id, @RequestBody WarriorDto warriorDto){
+    boolean ifexist=warriorService.ifWarriorExists(id);
+    if(!ifexist){
+        return new ResponseEntity<>((HttpStatus.NOT_FOUND));
+    }
+    WarriorEntity warriorEntity =warriorMapper.mapFrom(warriorDto);
+    warriorEntity.setId(id);
+    WarriorEntity savedWarrior=warriorService.CreateWarrior(warriorEntity);
+    return new ResponseEntity<>(warriorMapper.mapTo(savedWarrior),HttpStatus.OK);
+    }
+}
